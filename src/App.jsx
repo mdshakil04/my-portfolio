7c2d8b0e192321a3588173ab9dc3f9e55e18d847
mdshakil04 <m.shakil04@yahoo.com>
2023-12-10 22:38:30 +0600

    Components setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-7xl text-center text-cyan-400'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext, useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/new-logo.png"
+
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/about">About Me</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-cyan-500 font-bold  rounded-xl">
+      <div className="">
+        <img className="w-2/3" src={logo} alt="Shakil" />
+      </div>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <div className="items-center gap-2"></div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 text-lg">{navLinks}</ul>
+      </div>
+      <div className="navbar-end "></div>
+    </div>
+  );
+};
+
+export default Navbar;
