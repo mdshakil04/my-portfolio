93e8a6febfa3b30369975c050fd77015f54266c3
mdshakil04 <m.shakil04@yahoo.com>
2025-05-12 15:39:38 +0600

    Blob animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blob: {
+          "0%": { transform: "scale(1)" },
+          "33%": { transform: "scale(1.2)" },
+          "66%": { transform: "scale(0.8)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+      animation: {
+        blob: 'blob 5s infinite',
+      }
+    },
   },
   plugins: [require("daisyui")],
-}
-
+};
